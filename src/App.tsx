a7fe5e7b6417d8af7b1d24fbfa8ba22b5b743c78
henriquebazi <henriquebazi96@gmail.com>
2021-06-22 22:44:21 -0300

    feat: page home and new room

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import { Home } from './pages/Home'
+import { NewRoom } from './pages/NewRoom';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Oi mundo</h1>
-    </div>
+    <NewRoom />
   );
 }
 
